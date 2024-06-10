a18458dd706acb065294c4a6021f2de1f1898358
thaidui123 <vothongthai02012006@gmail.com>
2024-06-10 14:58:08 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace bài_tập_mới
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.btnthem = new System.Windows.Forms.Button();
+            this.lsbkq = new System.Windows.Forms.ListBox();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.btnsua = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(236, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(346, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Danh Sách Sinh Viên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(262, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(274, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập họ và tên sinh viên";
+            // 
+            // txtname
+            // 
+            this.txtname.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtname.Location = new System.Drawing.Point(92, 146);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(245, 38);
+            this.txtname.TabIndex = 2;
+            // 
+            // btnthem
+            // 
+            this.btnthem.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnthem.Location = new System.Drawing.Point(422, 146);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(114, 35);
+            this.btnthem.TabIndex = 3;
+            this.btnthem.Text = "Thêm";
+            this.btnthem.UseVisualStyleBackColor = true;
+            // 
+            // lsbkq
+            // 
+            this.lsbkq.FormattingEnabled = true;
+            this.lsbkq.Location = new System.Drawing.Point(84, 250);
+            this.lsbkq.Name = "lsbkq";
+            this.lsbkq.Size = new System.Drawing.Size(566, 147);
+            this.lsbkq.TabIndex = 4;
+            this.lsbkq.SelectedIndexChanged += new System.EventHandler(this.lsbkq_SelectedIndexChanged);
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnxoa.Location = new System.Drawing.Point(567, 146);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(114, 35);
+            this.btnxoa.TabIndex = 3;
+            this.btnxoa.Text = "xóa";
+            this.btnxoa.UseVisualStyleBackColor = true;
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // btnsua
+            // 
+            this.btnsua.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsua.Location = new System.Drawing.Point(567, 196);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(114, 35);
+            this.btnsua.TabIndex = 3;
+            this.btnsua.Text = "sửa";
+            this.btnsua.UseVisualStyleBackColor = true;
+            this.btnsua.Click += new System.EventHandler(this.btnsua_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lsbkq);
+            this.Controls.Add(this.btnsua);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.btnthem);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.Button btnthem;
+        private System.Windows.Forms.ListBox lsbkq;
+        private System.Windows.Forms.Button btnxoa;
+        private System.Windows.Forms.Button btnsua;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bài_tập_mới
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnthem_Click(object sender, EventArgs e)
+        {
+            lsbkq.Items.Add(btnthem.Text);
+            txtname.Text=string.Empty;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lsbkq.Items.Add("trần kim anh ");
+            lsbkq.Items.Add("lê phước hòa ");
+            lsbkq.Items.Add("trần kim giàu ");
+            lsbkq.Items.Add("trần kim anh thy ");
+
+
+
+
+
+
+
+
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            lsbkq.Items.RemoveAt(lsbkq.SelectedIndex);
+        }
+
+        private void btnsua_Click(object sender, EventArgs e)
+        {
+            if (lsbkq.SelectedIndex < 0) return;
+            lsbkq.Items[lsbkq.SelectedIndex]=txtname.Text;
+        }
+
+        private void lsbkq_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtname.Text = lsbkq.SelectedItem.ToString();
+        }
+    }
+}
